3945b8cce1053c93b25ef086c3b32ccecccecf1b
szewczykm <szewczykm94@gmail.com>
2020-05-02 17:08:17 +0200

    Adjusted main to make use of application logic root

--- src/main/java/com/mszewczyk/cp/Main.java
+++ src/main/java/com/mszewczyk/cp/Main.java
@@ -5,19 +5,25 @@ import com.mszewczyk.cp.configuration.yaml.JsonConfiguration;
 import com.mszewczyk.cp.controller.CommandController;
 import com.mszewczyk.cp.controller.CommandSource;
 import com.mszewczyk.cp.controller.configuration.ControllerConfiguration;
-import spark.Route;
+import com.mszewczyk.cp.controller.routes.RequestHandler;
+import com.mszewczyk.cp.service.AppLogicRoot;
 
 import java.nio.file.Path;
 
 public class Main {
-
-    public static final Route ROUTE = ((request, response) -> "OK");
-
     public static void main(String[] args) {
         JacksonMapDeserializer deserializer = new JacksonMapDeserializer();
         ControllerConfiguration configuration = new JsonConfiguration(Path.of("src\\main\\resources\\application.json"), deserializer);
-        //TODO: Replace with non stub routes
-        CommandSource source = new CommandController(configuration, ROUTE, ROUTE);
+        RequestHandler handler = new RequestHandler();
+        CommandSource source = new CommandController(configuration,
+                handler::handleAddToGroup, handler::removeFromGroup);
         source.initialize();
+
+        AppLogicRoot appLogicRoot = AppLogicRoot
+                .builder()
+                .commandProducer(handler)
+                .build();
+
+        appLogicRoot.wire();
     }
 }
