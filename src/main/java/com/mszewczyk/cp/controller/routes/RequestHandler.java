9d83cb7b7c2b7f767fa62ffa9395d13140512fbc
szewczykm <szewczykm94@gmail.com>
2020-05-02 17:23:11 +0200

    Implemented request handler logic

--- src/main/java/com/mszewczyk/cp/controller/routes/RequestHandler.java
+++ src/main/java/com/mszewczyk/cp/controller/routes/RequestHandler.java
@@ -9,8 +9,10 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.function.Consumer;
 
-// TODO: Implement handlers
 public class RequestHandler implements CommandProducer {
+    private static final String USER_PATH_VARIABLE = ":user";
+    private static final String GROUP_PATH_VARIABLE = ":group";
+    private static final int ACCEPTED_STATUS_CODE = 202;
     private final Collection<Consumer<Command>> commandConsumers;
 
     public RequestHandler() {
@@ -23,10 +25,28 @@ public class RequestHandler implements CommandProducer {
     }
 
     public String handleAddToGroup(Request request, Response response) {
-        return null;
+        processRequest(request, response, Command.Operation.ADD);
+        return "Added.";
     }
 
     public String removeFromGroup(Request request, Response response) {
-        return null;
+        processRequest(request, response, Command.Operation.REMOVE);
+        return "Removed.";
+    }
+
+    private void processRequest(Request request, Response response, Command.Operation add) {
+        Command addCommand = extractCommand(request, add);
+        populateToAllConsumers(addCommand);
+        response.status(ACCEPTED_STATUS_CODE);
+    }
+
+    private Command extractCommand(Request request, Command.Operation operation) {
+        String user = request.params(USER_PATH_VARIABLE);
+        String group = request.params(GROUP_PATH_VARIABLE);
+        return new Command(operation, user, group);
+    }
+
+    private void populateToAllConsumers(Command command) {
+        commandConsumers.forEach(consumer -> consumer.accept(command));
     }
 }
