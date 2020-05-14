65619aa5640e12e2d8186cefe022cdb360880217
szewczykm <szewczykm94@gmail.com>
2020-05-14 16:57:01 +0200

    Refactored kafka producer

--- src/main/java/com/mszewczyk/cp/producer/KafkaEventsProducer.java
+++ src/main/java/com/mszewczyk/cp/producer/KafkaEventsProducer.java
@@ -1,14 +1,42 @@
 package com.mszewczyk.cp.producer;
 
-import com.mszewczyk.cp.model.Command;
 import com.mszewczyk.cp.service.commands.CommandProducer;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
-// TODO: implement, use external model instead of the internal one
+import java.util.Properties;
+import java.util.function.Function;
+
 @Slf4j
-public class KafkaEventsProducer implements CommandProducer {
+public class KafkaEventsProducer<T> implements CommandProducer<T> {
+    private final Producer<String, String> producer;
+    private final Function<T, String> toStringMapper;
+    private final String topic;
+
+    public KafkaEventsProducer(Function<T, String> toStringMapper, ProducerConfiguration configuration) {
+        this.toStringMapper = toStringMapper;
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, configuration.brokers());
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, configuration.producerId());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producer =  new KafkaProducer<>(props);
+        topic = configuration.topic();
+    }
+
     @Override
-    public void produce(Command command) {
-        log.info("Event produced. [command={}]", command);
+    public void produce(T value) {
+        log.info("Event produced. [value={}]", value);
+        ProducerRecord<String, String> producerRecord = getProducerRecord(value);
+        producer.send(producerRecord);
+    }
+
+    private ProducerRecord<String, String> getProducerRecord(T value) {
+        String serializedMessage = toStringMapper.apply(value);
+        return new ProducerRecord<>(topic, serializedMessage);
     }
 }
