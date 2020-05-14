ae430803618305ac7026654bd4ed5d207a52cb8c
szewczykm <szewczykm94@gmail.com>
2020-05-14 18:47:34 +0200

    Added routing by group id

--- src/main/java/com/mszewczyk/cp/producer/KafkaEventsProducer.java
+++ src/main/java/com/mszewczyk/cp/producer/KafkaEventsProducer.java
@@ -15,28 +15,30 @@ import java.util.function.Function;
 public class KafkaEventsProducer<T> implements CommandProducer<T> {
     private final Producer<String, String> producer;
     private final Function<T, String> toStringMapper;
+    private final Function<T, String> keyExtractor;
     private final String topic;
 
-    public KafkaEventsProducer(Function<T, String> toStringMapper, ProducerConfiguration configuration) {
+    public KafkaEventsProducer(Function<T, String> toStringMapper, Function<T, String> keyExtractor, ProducerConfiguration configuration) {
         this.toStringMapper = toStringMapper;
+        this.keyExtractor = keyExtractor;
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, configuration.brokers());
         props.put(ProducerConfig.CLIENT_ID_CONFIG, configuration.producerId());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        producer =  new KafkaProducer<>(props);
+        producer = new KafkaProducer<>(props);
         topic = configuration.topic();
     }
 
     @Override
     public void produce(T value) {
         log.info("Event produced. [value={}]", value);
-        ProducerRecord<String, String> producerRecord = getProducerRecord(value);
+        ProducerRecord<String, String> producerRecord = getProducerRecord(value, keyExtractor.apply(value));
         producer.send(producerRecord);
     }
 
-    private ProducerRecord<String, String> getProducerRecord(T value) {
+    private ProducerRecord<String, String> getProducerRecord(T value, String key) {
         String serializedMessage = toStringMapper.apply(value);
-        return new ProducerRecord<>(topic, serializedMessage);
+        return new ProducerRecord<>(topic, key, serializedMessage);
     }
 }
